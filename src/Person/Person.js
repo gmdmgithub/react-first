6cf27ffb458fdd2c02c0443816bff4cc90bf6dfb
gmdmgithub <gmika@interia.pl>
2019-02-06 21:23:59 +0100

    css added to component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Person.css";
 
 const person = props => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>
         Hi my name is {props.name} I like {Math.floor(Math.random() * 100)}{" "}
         number, I'm {props.age}
